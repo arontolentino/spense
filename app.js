25bd438ed1cf8e7dad68b33a99078517d7bb1621
Aron Tolentino <arontolentino@outlook.com>
2020-01-10 16:24:30 -0500

    Update DOM:  total income, expense, savings with new entry

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
 $(document).ready(function() {
-	let totalSavings;
-	let totalExpenses = 0.0;
-	let totalIncomes = 0.0;
+	let totalSavings = 0;
+	let totalExpenses = 0;
+	let totalIncomes = 0;
 
-	let expenses = [
-		{
-			date: new Date(),
-			description: 'test exepense 1',
-			amount: 18.9
-		},
-		{
-			date: new Date(),
-			description: 'test exepense 2',
-			amount: 49.9
-		},
-		{
-			date: new Date(),
-			description: 'test exepense 3',
-			amount: 129.9
-		}
-	];
+	let expenses = [];
 
-	let incomes = [
-		{
-			date: new Date(),
-			description: 'test income 1',
-			amount: 103.83
-		},
-		{
-			date: new Date(),
-			description: 'test income 1',
-			amount: 130.29
-		},
-		{
-			date: new Date(),
-			description: 'test income 1',
-			amount: 140.21
-		}
-	];
+	let incomes = [];
 
 	// Add all expense transactions
 
@@ -45,7 +13,8 @@ $(document).ready(function() {
 		expenses.forEach(expense => {
 			totalExpenses += expense.amount;
 		});
-		console.log(totalExpenses);
+
+		$('.totalExpenses p').text(`$${totalExpenses.toFixed(2)}`);
 	};
 
 	// Add all income transactions
@@ -54,14 +23,16 @@ $(document).ready(function() {
 		incomes.forEach(income => {
 			totalIncomes += income.amount;
 		});
-		console.log(totalIncomes);
+
+		$('.totalIncome p').text(`$${totalIncomes.toFixed(2)}`);
 	};
 
 	// Calculate total savings
 
 	const calcSavings = () => {
 		totalSavings = totalIncomes - totalExpenses;
-		console.log(totalSavings);
+		$('.totalSavings p').text(`$${totalSavings.toFixed(2)}`);
+		$;
 	};
 
 	// Create new entry
@@ -69,19 +40,21 @@ $(document).ready(function() {
 		let entry = {
 			date: $('#inputDate').val(),
 			description: $('#inputDesc').val(),
-			amount: $('#inputAmount').val()
+			amount: parseInt($('#inputAmount').val())
 		};
 
 		let entryType = $('#inputType').val();
 
+		console.log(entry);
+
 		if (entryType === 'Income') {
 			incomes.push(entry);
-			console.log(incomes);
+			addIncomes();
 		} else if (entryType === 'Expense') {
 			expenses.push(entry);
-			console.log(expenses);
+			addExpenses();
 		}
 
-		console.log(entry);
+		calcSavings();
 	});
 });
